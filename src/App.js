6d686500e2b4ec19ba2b5be5cafda6f211e53011
Filip <f.kaczmarek1@wp.pl>
2021-10-02 11:23:38 +0200

    FullPageLoader Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+import FullPageLoader from './Components/FullPageLoader'
+
+export class App extends React.Component {
+  state = {
+    // global state
+    isLoading: false,
+    hasError: false,
+    errorMessage: '',
+    isInfoDisplayed: false,
+    infoMessage: '',
+
+    // user/auth stane
+    isUserLoggedIn: false,
+    userDisplayName: '',
+    userEmail: '',
+    userAvatar: '',
+
+    // router state
+    notLoginUserRoute: 'LOGIN', // 'NEW-ACCOUNT' or 'FORGOT PASSWORD'
+
+    // login page state
+    loginEmail: '',
+    loginPassword: '',
+
+    // create account page state
+    createAccountEmail: '',
+    createAccountPassword: '',
+    createAccountRepeatPassword: '',
+
+    // recover password state
+    recoverPasswordEmail: '',
+
+    // course list page
+    courses: null,
+    searchPhrase: '',
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>TEXT</h1>
+        <FullPageLoader />
+      </div>
+    );
+  }
 }
 
 export default App;
