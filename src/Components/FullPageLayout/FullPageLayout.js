74cab12dbb6db5d6ab0ffc3898f7f9db88cd353a
Filip <f.kaczmarek1@wp.pl>
2021-10-07 17:32:07 +0200

    FullPageLayout Component Added

---
+++ src/Components/FullPageLayout/FullPageLayout.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styles from './styles.module.css'
+
+export const FullPageLayout = (props) => {
+    const { className, children, ...otherProps } = props
+
+    return (
+        <div
+            className={`${styles.root}${className ? ` ${className}` : ''}`}
+            {...otherProps}
+            >
+                {children}
+            </div>
+    )
+}
+
+FullPageLayout.propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+}
+
+export default FullPageLayout
