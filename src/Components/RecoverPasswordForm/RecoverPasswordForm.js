60fe71b44d8d156cc9a84eea16ca08a26afe270e
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2021-10-30 11:02:10 +0200

    Recover Password Form component created

---
+++ src/Components/RecoverPasswordForm/RecoverPasswordForm.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Logo from '../Logo'
+import Typography from '../Typography'
+import TextField from '../TextField'
+import Button from '../Button'
+
+import styles from './styles.module.css'
+
+export const RecoverPasswordForm = (props) => {
+  const {
+    className,
+    email,
+    onChangeEmail,
+    onClickRecover,
+    onClickBackToLogin,
+    ...otherProps
+  } = props
+
+  return (
+    <div
+      className={`${styles.root}${className ? ` ${className}` : ''}`}
+      {...otherProps}
+    >
+      <Logo
+        className={styles.logo}
+      />
+      <Typography
+        className={styles.header}
+        variant={'h1'}
+      >
+        Recover Password
+      </Typography>
+      <TextField
+        className={styles.textField}
+        placeholder={'E-mail'}
+        value={email}
+        onChange={onChangeEmail}
+      />
+      <Button
+        variant={'contained'}
+        color={'primary'}
+        className={styles.button}
+        onClick={onClickRecover}
+      >
+        RECOVER
+      </Button>
+      <Button
+        variant={'text'}
+        className={styles.button}
+        onClick={onClickBackToLogin}
+      >
+        BACK TO LOGIN
+      </Button>
+    </div>
+  )
+}
+
+RecoverPasswordForm.propTypes = {
+  className: PropTypes.string,
+  email: PropTypes.string.isRequired,
+  onChangeEmail: PropTypes.func.isRequired,
+  onClickRecover: PropTypes.func.isRequired,
+  onClickBackToLogin: PropTypes.func.isRequired,
+}
+
+export default RecoverPasswordForm
